e4c2e92a7b11f642f4627e53f549233834a9607e
fzyier <babeckiy2004@gmail.com>
2023-06-14 01:14:38 +0300

    done mediator design pattern

--- Behavioral/Mediator/mediator.cpp
+++ Behavioral/Mediator/mediator.cpp
@@ -1,2 +1,26 @@
 #include "mediator.hpp"
 
+// ! User
+void User::send(const std::string& message)
+{
+    mediator_->sendMessage(this, message);
+}
+void User::receive(const std::string& sender, const std::string& message) 
+{
+    std::cout << name_ << " received a message from " << sender << ": " << message << std::endl;
+}
+// ! Chatroom
+void Chatroom::addColleague(Colleague* colleague) 
+{
+    colleagues.push_back(colleague);
+}
+void Chatroom::sendMessage(Colleague* sender, const std::string& message)  
+{
+    for (Colleague* colleague : colleagues) 
+    {
+        if (colleague != sender) 
+        {
+            colleague->receive(sender->name, message);
+        }
+    }
+}
--- Behavioral/Mediator/mediator.hpp
+++ Behavioral/Mediator/mediator.hpp
@@ -1,3 +1,39 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <vector>
 
+class Colleague;
+
+class Mediator {
+public:
+    virtual void sendMessage(Colleague* sender, const std::string& message) = 0;
+};
+
+class Colleague 
+{
+public:
+    Mediator* mediator_;
+    std::string name_;
+
+    Colleague(Mediator* mediator, const std::string& name) : mediator_(mediator), name_(name) {}
+    virtual void send(const std::string& message) = 0;
+    virtual void receive(const std::string& sender, const std::string& message) = 0;
+};
+
+class User : public Colleague 
+{
+public:
+    User(Mediator* mediator, const std::string& name) : Colleague(mediator, name) {}
+    void send(const std::string& message) override;
+    void receive(const std::string& sender, const std::string& message) override;
+};
+
+class Chatroom : public Mediator 
+{
+public:
+    std::vector<Colleague*> colleagues;
+    void addColleague(Colleague* colleague);
+    void sendMessage(Colleague* sender, const std::string& message) override;
+};
