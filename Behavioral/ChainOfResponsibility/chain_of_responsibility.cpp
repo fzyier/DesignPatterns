bd36eb8f8a0c482991bb0c5b4f09ef18bdf93f5d
fzyier <babeckiy2004@gmail.com>
2023-06-13 18:03:03 +0300

    done chain of responsibility design pattern

--- Behavioral/ChainOfResponsibility/chain_of_responsibility.cpp
+++ Behavioral/ChainOfResponsibility/chain_of_responsibility.cpp
@@ -1,2 +1,45 @@
 #include "chain_of_responsibility.hpp"
 
+// ! Logger
+void Logger::setNextLogger(Logger* logger) 
+{
+  nextLogger = logger;
+}
+void Logger::logMessage(const std::string& message, int level) 
+{
+  if (level >= getLogLevel()) 
+  {
+      writeLog(message);
+  }
+  if (nextLogger != nullptr) 
+  {
+      nextLogger->logMessage(message, level);
+  }
+}
+// ! InfoLogger
+int InfoLogger::getLogLevel() 
+{
+  return 1;
+}
+void InfoLogger::writeLog(const std::string& message) 
+{
+  std::cout << "Info Logger: " << message << '\n';
+}
+// ! WarningLogger
+int WarningLogger::getLogLevel() 
+{
+  return 2;
+}
+void WarningLogger::writeLog(const std::string& message) 
+{
+  std::cout << "Warning Logger: " << message << '\n';
+}
+// ! ErrorLogger
+int ErrorLogger::getLogLevel()  
+{
+  return 3;
+}
+void ErrorLogger::writeLog(const std::string& message)  
+{
+  std::cout << "Error Logger: " << message << '\n';
+}
--- Behavioral/ChainOfResponsibility/chain_of_responsibility.hpp
+++ Behavioral/ChainOfResponsibility/chain_of_responsibility.hpp
@@ -1,2 +1,42 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+
+// Handler
+class Logger 
+{
+protected:
+  Logger* nextLogger;
+
+public:
+  Logger() : nextLogger(nullptr) {}
+  void setNextLogger(Logger* logger);
+  void logMessage(const std::string& message, int level);
+
+protected:
+  virtual int getLogLevel() = 0;
+  virtual void writeLog(const std::string& message) = 0;
+};
+
+// Concrete Handlers
+class InfoLogger : public Logger 
+{
+protected:
+  int getLogLevel() override;
+  void writeLog(const std::string& message) override;
+};
+
+class WarningLogger : public Logger 
+{
+protected:
+  int getLogLevel() override;
+  void writeLog(const std::string& message) override;
+};
+
+class ErrorLogger : public Logger 
+{
+protected:
+  int getLogLevel() override;
+  void writeLog(const std::string& message) override;
+};
