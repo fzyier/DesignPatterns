269c90916feff88053cabf97a2fe19a08062e571
fzyier <babeckiy2004@gmail.com>
2023-06-14 02:35:51 +0300

    done memento design pattern

--- Behavioral/Memento/memento.cpp
+++ Behavioral/Memento/memento.cpp
@@ -1,2 +1,26 @@
 #include "memento.hpp"
 
+// ! Data
+std::string Data::get_name() const 
+{
+    return name_;
+}
+// ! Saver
+void Saver::add_memento(const Data& memento) 
+{
+    data_.push_back(memento);
+}
+Data Saver::get_memento(int index) const 
+{
+    return data_[index];
+}
+// ! Castle
+void Castle::set_name(const std::string& name) 
+{
+    this->king_name_ = name;
+    saver_.add_memento(Data(name));
+}
+std::string Castle::get_name() const 
+{
+    return king_name_;
+}
--- Behavioral/Memento/memento.hpp
+++ Behavioral/Memento/memento.hpp
@@ -1,3 +1,35 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <vector>
 
+class Data 
+{
+private:
+    std::string name_;
+
+public:
+    Data(const std::string& name) : name_(name) {}
+    std::string get_name() const;
+};
+
+class Saver 
+{
+private:
+    std::vector<Data> data_;
+public:
+    Saver();
+    void add_memento(const Data& memento);
+    Data get_memento(int index) const;
+};
+
+class Castle 
+{
+private:
+    std::string king_name_;
+    Saver saver_;
+public:
+    void set_name(const std::string& name);
+    std::string get_name() const;
+};
