b3b0bd62ffac111e0bda76ff8114672933712e20
fzyier <babeckiy2004@gmail.com>
2023-06-13 19:02:40 +0300

    Done command design pattern

--- Behavioral/Command/command.cpp
+++ Behavioral/Command/command.cpp
@@ -1,2 +1,33 @@
 #include "command.hpp"
 
+// ! Light
+void Light::turnOn() 
+{
+    std::cout << "Light is on" << '\n';
+}
+void Light::turnOff() 
+{
+    std::cout << "Light is off" << '\n';
+}
+// ! TurnOnCommand
+void TurnOnCommand::execute() 
+{
+    light_->turnOn();
+}
+// ! TurnOffCommand
+void TurnOffCommand::TurnOffCommand::execute() 
+{
+    light_->turnOff();
+}
+// ! RemoteControl
+void RemoteControl::addCommand(Command* command) 
+{
+    commands_.push_back(command);
+}
+void RemoteControl::pressButton(int index) 
+{
+    if (index >= 0 && index < commands_.size()) 
+    {
+        commands_[index]->execute();
+    }
+}
--- Behavioral/Command/command.hpp
+++ Behavioral/Command/command.hpp
@@ -1,3 +1,50 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <vector>
 
+class Light 
+{
+public:
+    void turnOn();
+    void turnOff();
+};
+
+class Command
+ {
+public:
+    virtual void execute() = 0;
+};
+
+class TurnOnCommand : public Command
+{
+private:
+    Light* light_;
+
+public:
+    TurnOnCommand(Light* light) : light_(light) {}
+
+    void execute() override;
+};
+
+class TurnOffCommand : public Command 
+{
+private:
+    Light* light_;
+
+public:
+    TurnOffCommand(Light* light) : light_(light) {}
+
+    void execute() override;
+};
+
+class RemoteControl 
+{
+private:
+    std::vector<Command*> commands_;
+
+public:
+    void addCommand(Command* command);
+    void pressButton(int index);
+};
