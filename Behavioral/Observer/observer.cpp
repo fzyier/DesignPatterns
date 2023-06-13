686ac521f354e5df4bc5c0c365a8c46d04dbfd7a
fzyier <babeckiy2004@gmail.com>
2023-06-14 02:35:24 +0300

    Done observer design pattern

--- Behavioral/Observer/observer.cpp
+++ Behavioral/Observer/observer.cpp
@@ -1,2 +1,44 @@
 #include "observer.hpp"
 
+// ! WeatherStation
+void WeatherStation::notify() 
+{
+    for (Observer* observer : observers_) 
+    {
+        observer->update(temperature_);
+    }
+}
+
+void WeatherStation::add_observer(Observer* observer) 
+{
+    observers_.push_back(observer);
+}
+
+void WeatherStation::remove_observer(Observer* observer) 
+{
+    for (auto it = observers_.begin(); it != observers_.end(); ++it) 
+    {
+        if (*it == observer) {
+            observers_.erase(it);
+            break;
+        }
+    }
+}
+
+void WeatherStation::set_temperature(float newTemperature) 
+{
+    temperature_ = newTemperature;
+    notify();
+}
+
+// ! Display
+void Display::update(float temperature) 
+{
+    std::cout << "Display: The current temperature is " << temperature << " degrees Celsius." << std::endl;
+}
+
+// ! Logger
+void Logger::update(float temperature) 
+{
+    std::cout << "Logger: Recording temperature: " << temperature << " degrees Celsius." << std::endl;
+}
--- Behavioral/Observer/observer.hpp
+++ Behavioral/Observer/observer.hpp
@@ -1,3 +1,35 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
 
+class Observer 
+{
+public:
+    virtual void update(float temperature) = 0;
+};
+
+class WeatherStation 
+{
+private:
+    std::vector<Observer*> observers_;
+    float temperature_;
+
+    void notify();
+public:
+    void add_observer(Observer* observer);
+    void remove_observer(Observer* observer);
+    void set_temperature(float newTemperature);
+};
+
+class Display 
+{
+public:
+    void update(float temperature);
+};
+
+class Logger 
+{
+public:
+    void update(float temperature);
+};
