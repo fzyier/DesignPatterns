c7c36062c73696cbbae1d4e1bd0cff16746fc057
fzyier <babeckiy2004@gmail.com>
2023-06-14 03:06:28 +0300

    done state design pattern

--- Behavioral/State/state.cpp
+++ Behavioral/State/state.cpp
@@ -1,2 +1,39 @@
 #include "state.hpp"
 
+// ! TrafficLight
+TrafficLight::TrafficLight() 
+{
+    current_state_ = new GreenState();
+}
+
+void TrafficLight::set_state(State* state) 
+{
+    delete current_state_;
+    current_state_ = state;
+}
+
+void TrafficLight::perform_action() 
+{
+    current_state_->perform_action(this);
+}
+
+// ! GreenState
+void GreenState::perform_action(TrafficLight* trafficLight) 
+{
+    std::cout << "Green Light: Go!" << std::endl;
+    trafficLight->set_state(new YellowState());
+}
+
+// ! YellowState
+void YellowState::perform_action(TrafficLight* trafficLight) 
+{
+    std::cout << "Yellow Light: Prepare to stop." << std::endl;
+    trafficLight->set_state(new RedState());
+}
+
+// ! RedState
+void RedState::perform_action(TrafficLight* trafficLight) 
+{
+    std::cout << "Red Light: Stop!" << std::endl;
+    trafficLight->set_state(new GreenState());
+}
--- Behavioral/State/state.hpp
+++ Behavioral/State/state.hpp
@@ -1,3 +1,38 @@
 #pragma once
 
+#include <iostream>
 
+class TrafficLight 
+{
+private:
+    class State* current_state_;
+public:
+    TrafficLight();
+
+    void set_state(State* state);
+    void perform_action();
+};
+
+class State 
+{
+public:
+    virtual void perform_action(TrafficLight* trafficLight) = 0;
+};
+
+class GreenState : public State 
+{
+public:
+    void perform_action(TrafficLight* trafficLight) override;
+};
+
+class YellowState : public State 
+{
+public:
+    void perform_action(TrafficLight* trafficLight) override;
+};
+
+class RedState : public State 
+{
+public:
+    void perform_action(TrafficLight* trafficLight) override;
+};
