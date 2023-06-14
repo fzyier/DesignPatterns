d3b6fb9f810e414e65216ccf071c1f20e00c8f7b
fzyier <babeckiy2004@gmail.com>
2023-06-14 03:37:52 +0300

    done template_method design pattern

--- Behavioral/TemplateMethod/template_method.cpp
+++ Behavioral/TemplateMethod/template_method.cpp
@@ -1,2 +1,38 @@
 #include "template_method.hpp"
 
+// ! HotBeverage
+void HotBeverage::prepare_beverage() 
+{
+    boil_water();
+    brew();
+    pour_into_cup();
+    add_condiments();
+}
+void HotBeverage::boil_water() 
+{
+    std::cout << "Boiling water..." << std::endl;
+}
+void HotBeverage::pour_into_cup() 
+{
+    std::cout << "Pouring into cup..." << std::endl;
+}
+
+// ! Coffee
+void Coffee::brew()  
+{
+    std::cout << "Brewing coffee..." << std::endl;
+}
+void Coffee::add_condiments()  
+{
+    std::cout << "Adding sugar and milk..." << std::endl;
+}
+
+// ! Tea
+void Tea::brew()  
+{
+    std::cout << "Steeping tea leaves..." << std::endl;
+}
+void Tea::add_condiments()  
+{
+    std::cout << "Adding lemon..." << std::endl;
+}
--- Behavioral/TemplateMethod/template_method.hpp
+++ Behavioral/TemplateMethod/template_method.hpp
@@ -1,3 +1,25 @@
-#pragma once
+#include <iostream>
 
+class HotBeverage 
+{
+public:
+    void prepare_beverage();
+    virtual void brew() = 0;
+    virtual void add_condiments() = 0;
+    void boil_water();
+    void pour_into_cup();
+};
 
+class Coffee : public HotBeverage 
+{
+public:
+    void brew() override;
+    void add_condiments() override;
+};
+
+class Tea : public HotBeverage 
+{
+public:
+    void brew() override;
+    void add_condiments() override;
+};
