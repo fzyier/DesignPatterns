12958a6fea0a5107c91f2b4d157f7d0beef64731
fzyier <babeckiy2004@gmail.com>
2023-06-14 04:15:01 +0300

    done visitor design pattern

--- Behavioral/Visitor/visitor.cpp
+++ Behavioral/Visitor/visitor.cpp
@@ -1,2 +1,15 @@
 #include "visitor.hpp"
 
+// ! AreaVisitor
+void AreaVisitor::visit(Circle& circle)  
+{
+    std::cout << "Calculating area of Circle." << '\n';
+}
+void AreaVisitor::visit(Rectangle& rectangle)  
+{
+    std::cout << "Calculating area of Rectangle." << '\n';
+}
+void AreaVisitor::visit(Triangle& triangle)  
+{
+    std::cout << "Calculating area of Triangle." << '\n';
+}
--- Behavioral/Visitor/visitor.hpp
+++ Behavioral/Visitor/visitor.hpp
@@ -1,3 +1,46 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
 
+class ShapeVisitor;
+
+class Shape 
+{
+public:
+    virtual void accept(ShapeVisitor& visitor) = 0;
+};
+
+class Circle : public Shape 
+{
+public:
+    void accept(ShapeVisitor& visitor) override;
+};
+
+class Rectangle : public Shape 
+{
+public:
+    void accept(ShapeVisitor& visitor) override;
+};
+
+class Triangle : public Shape 
+{
+public:
+    void accept(ShapeVisitor& visitor) override;
+};
+
+class ShapeVisitor 
+{
+public:
+    virtual void visit(Circle& circle) = 0;
+    virtual void visit(Rectangle& rectangle) = 0;
+    virtual void visit(Triangle& triangle) = 0;
+};
+
+class AreaVisitor : public ShapeVisitor 
+{
+public:
+    void visit(Circle& circle) override;
+    void visit(Rectangle& rectangle) override;
+    void visit(Triangle& triangle) override;
+};
