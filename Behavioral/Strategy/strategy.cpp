a8712f000abc9a412625898c51f82772e7d30041
fzyier <babeckiy2004@gmail.com>
2023-06-14 03:16:41 +0300

    Done strategy design pattern

--- Behavioral/Strategy/strategy.cpp
+++ Behavioral/Strategy/strategy.cpp
@@ -1,2 +1,21 @@
 #include "strategy.hpp"
 
+// ! BubbleSortStrategy
+void BubbleSortStrategy::sort(std::vector<int>& data) override 
+{
+    std::cout << "Sorting using Bubble Sort." << std::endl;
+}
+// ! QuickSortStrategy
+void QuickSortStrategy::sort(std::vector<int>& data) override 
+{
+    std::cout << "Sorting using Quick Sort." << std::endl;
+}
+// ! Sorter
+void Sorter::set_sorting_strategy(SortingStrategy* strategy) 
+{
+    sorting_strategy_ = strategy;
+}
+void Sorter::perform_sort(std::vector<int>& data) 
+{
+    sorting_strategy_->sort(data);
+}
--- Behavioral/Strategy/strategy.hpp
+++ Behavioral/Strategy/strategy.hpp
@@ -1,3 +1,33 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+class SortingStrategy 
+{
+public:
+    virtual void sort(std::vector<int>& data) = 0;
+};
+
+class BubbleSortStrategy : public SortingStrategy 
+{
+public:
+    void sort(std::vector<int>& data) override;
+};
+
+class QuickSortStrategy : public SortingStrategy 
+{
+public:
+    void sort(std::vector<int>& data) override;
+};
+
+class Sorter 
+{
+private:
+    SortingStrategy* sorting_strategy_;
+public:
+    Sorter(SortingStrategy* strategy) : sorting_strategy_(strategy) {}
+    void set_sorting_strategy(SortingStrategy* strategy);
+    void perform_sort(std::vector<int>& data);
+};
